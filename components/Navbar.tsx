3392c8a3b44f943cadc14676cb0c02ad520bcd90
Manit Mishra <mshmanit@gmail.com>
2025-11-10 15:16:07 -0500

    minor changes, first look at proj

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -43,18 +43,18 @@ export default function Navbar() {
               >
                 Make Template
               </Link>
-              <Link
+              {/* <Link
                 href="/dashboard/swipe"
                 className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
               >
                 Swipe
-              </Link>
-              <Link
+              </Link> */}
+              {/* <Link
                 href="/mentorship"
                 className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
               >
                 Mentorship
-              </Link>
+              </Link> */}
             </div>
           </div>
           <div className="flex items-center space-x-4">
